cd2bba7a40320ae014192bf28c65fa01e4089f20
Guilherme Lucio <guilhermelucio.design@gmail.com>
2018-01-13 15:41:33 +0000

    Decouple the node_module files from the server bundle

--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const merge = require('webpack-merge');
+const webpackNodeExternals = require('webpack-node-externals');
 const baseConfig = require('./webpack.config');
 
 const config = {
@@ -16,7 +17,16 @@ const config = {
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'build')
-    }
+    },
+
+    // When running webpack in the server, that has support to load modules
+    // using CMD already, it's a good idea not to include the imports as React,
+    // Express and so on at the bundle.js, since they can be loaded asynchronously
+    // To do this the webpack-node-externals must be called, which means
+    // any module inside of the node_modules folder won't be bundled on the output file.
+    // Since the bundle.js file is never sent back to the user, this might not be necessary,
+    // there is no problem having them bundled, but... if one wishes 
+    externals: [webpackNodeExternals()]
     
 };
 
