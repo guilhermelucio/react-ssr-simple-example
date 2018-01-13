ff02f3acb5e2f2e3b6de72aeba23029c977297cf
Guilherme Lucio <guilhermelucio.design@gmail.com>
2018-01-13 17:15:46 +0000

    Redux on the client

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 import Routes from './Routes';
+import rootReducer from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 // Rendering the app on the client, this will replace the content
 // of the file the server created and React will take care of the application from there
@@ -13,8 +19,10 @@ import Routes from './Routes';
 // Warning: render(): Calling ReactDOM.render() to hydrate server-rendered markup will stop working 
 // in React v17. Replace the ReactDOM.render() call with ReactDOM.hydrate() if you want React to attach to the server HTML.
 ReactDOM.hydrate(
-    <BrowserRouter>
-        <Routes />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <Routes />
+        </BrowserRouter>
+    </Provider>,
     document.querySelector('#app')
 );
